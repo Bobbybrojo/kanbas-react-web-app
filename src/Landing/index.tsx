5627f752fcc56c5f53562d0cad34224221cafc9a
Bobbybrojo <43765519+Bobbybrojo@users.noreply.github.com>
2024-09-24 21:15:14 -0400

    Added section number to landing page

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -5,6 +5,7 @@ export default function Landing() {
     <>
       <h1>Assignment 1 Landing Page</h1>
       <h3>Name: Roberto Rojo</h3>
+      <h3>Section: 01</h3>
       <div>
         <h3>Link to Labs and Kanbas:</h3>
         <TOC />
